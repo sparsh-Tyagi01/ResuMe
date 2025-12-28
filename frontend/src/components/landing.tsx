08c4f97f728622fe9c0b15856f8f9a77703c58c1
Sparsh-tyagi01 <sparshtyagi630@gmail.com>
2025-12-29 02:42:02 +0530

    assemble landing page components

---
+++ frontend/src/components/landing.tsx
@@ -0,0 +1,23 @@
+import Footer from "./footer"
+import Homepage from "./homepage"
+import Navbar from "./navbar"
+
+const Landing = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <header className="sticky top-0 left-0 z-50 ">
+        <Navbar />
+      </header>
+
+      <main className="flex-1 w-full">
+        <Homepage/>
+      </main>
+
+      <footer className="w-full mt-10">
+        <Footer/>
+      </footer>
+    </div>
+  )
+}
+
+export default Landing

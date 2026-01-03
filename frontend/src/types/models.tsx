64326f359e9d074e7416a08c862610a17e7e8fa7
Sparsh-tyagi01 <sparshtyagi630@gmail.com>
2026-01-03 15:05:22 +0530

    add resume interface

--- frontend/src/types/models.tsx
+++ frontend/src/types/models.tsx
@@ -14,3 +14,32 @@ export interface work {
     bgColor: string;
     content: string;
 }
+
+export interface resume {
+    _id: string;
+    userId: string;
+    name: string;
+    email: string;
+    phone: string;
+    github?: string;
+    linkedin?: string;
+    skills: string[];
+    experience: experience[];
+    education: education[];
+    resumeUrl: string;
+    createdAt: string;
+    updatedAt: string;
+}
+
+export interface experience {
+    company: string;
+    role: string;
+    duration: string;
+    description: string;
+}
+
+export interface education {
+    degree: String,
+    institution: String,
+    year: String
+}
